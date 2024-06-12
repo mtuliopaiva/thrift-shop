51147e8048130b3170029113d8d39a1bd99300da
Marco Tulio Paiva <tulio.paiva10@gmail.com>
2024-06-11 22:59:16 -0300

    Working on LP

--- src/components/LayoutBaseLandingPage/index.tsx
+++ src/components/LayoutBaseLandingPage/index.tsx
@@ -1,44 +1,32 @@
-import React, { useState } from "react";
-import { Avatar, Button, Layout, Menu, Popconfirm, Dropdown } from "antd";
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  DashboardOutlined,
-  SettingOutlined,
-  SkinOutlined,
-  UserOutlined,
-  PoweroffOutlined,
-} from "@ant-design/icons";
+import React from "react";
+import { Avatar, Layout, Menu, Popconfirm, Dropdown } from "antd";
+
 import { useNavigate } from "react-router-dom";
-import Sider from "antd/es/layout/Sider";
-import Logo from "../../../public/logo-Thrift-shop.png";
-import ImgComponent from "../Img";
-import ProfileImage from "../../../public/profile-image.png";
 
 const { Header, Content, Footer } = Layout;
 
 const items = [
-    {
-      key: 1,
-      label: 'Home',
-      path: '/home'
-    },
-    {
-      key: 2,
-      label: 'Quem somos',
-      path: '/quem-somos'
-    },
-    {
-        key: 3,
-        label: 'Quem somos',
-        path: '/quem-somos'
-      },
-      {
-        key: 4,
-        label: 'Quem somos',
-        path: '/quem-somos'
-      }
-  ];
+  {
+    key: 1,
+    label: "Home",
+    path: "/home",
+  },
+  {
+    key: 2,
+    label: "Quem somos",
+    path: "/quem-somos",
+  },
+  {
+    key: 3,
+    label: "Quem somos",
+    path: "/quem-somos",
+  },
+  {
+    key: 4,
+    label: "Quem somos",
+    path: "/quem-somos",
+  },
+];
 
 const LayoutBaseLandingPage = ({ children }: { children: React.ReactNode }) => {
   const navigate = useNavigate();
@@ -47,47 +35,6 @@ const LayoutBaseLandingPage = ({ children }: { children: React.ReactNode }) => {
     console.log("Logout confirmado");
     navigate("/");
   };
-
-  const menuItems = [
-    {
-      key: "1",
-      icon: <DashboardOutlined />,
-      label: "Dashboard",
-      path: "/admin/dashboard",
-    },
-    {
-      key: "2",
-      icon: <SkinOutlined />,
-      label: "Products",
-      path: "/admin/products",
-    },
-    {
-      key: "3",
-      icon: <UserOutlined />,
-      label: "Clients",
-      path: "/admin/clients",
-    },
-    {
-      key: "4",
-      icon: <SettingOutlined />,
-      label: "Settings",
-      path: "/admin/settings",
-    },
-    {
-      key: "6",
-      icon: <PoweroffOutlined />,
-      label: (
-        <Popconfirm
-          title="Tem certeza que deseja sair?"
-          onConfirm={handleLogout}
-          okText="Sim"
-          cancelText="Não"
-        >
-          <span>Logout</span>
-        </Popconfirm>
-      ),
-    },
-  ];
 
   const profileMenuItems = [
     {
@@ -114,48 +61,86 @@ const LayoutBaseLandingPage = ({ children }: { children: React.ReactNode }) => {
   ];
 
   return (
-    <Layout className="h-screen w-full">
-
-      <Layout className="w-full h-full">
+    <Layout className="min-h-screen w-full">
       <Header className="flex items-center h-[80px] bg-[#A8E7DD]">
-      {/* Div 1: Imagem de Teste */}
-      <div className="flex items-center justify-start w-1/3">
-        <img
-          src="../../../public/logo-store.png"
-          alt="Logo da Thrift shop"
-          className="w-[40%]"
-        />
-      </div>
+        {/* Div 1: Imagem de Teste */}
+        <div className="flex items-center justify-start w-1/3">
+          <img
+            src="../../../public/logo-store.png"
+            alt="Logo da Thrift shop"
+            className="w-[40%]"
+          />
+        </div>
 
-      {/* Div 2: Menu Centralizado */}
-      <div className="flex justify-center w-1/3">
-        <Menu
-          mode="horizontal"
-          defaultSelectedKeys={["1"]}
-          items={items}
-          style={{ backgroundColor: "#A8E7DD", borderBottom: 'none' }}
-        />
-      </div>
+        {/* Div 2: Menu Centralizado */}
+        <div className="flex justify-center w-1/3">
+          <Menu
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            items={items}
+            style={{ backgroundColor: "#A8E7DD", borderBottom: "none" }}
+          />
+        </div>
 
-      {/* Div 3: Dropdown */}
-      <div className="flex items-center justify-end w-1/3 space-x-4 mr-4 cursor-pointer">
-        <Dropdown menu={{ items: profileMenuItems }} placement="bottomRight">
-          <div className="flex items-center">
-            <Avatar
-              src="https://via.placeholder.com/40"
-              style={{ border: "1px solid #B2F6E9" }}
-            />
-            <p className="m-0 ml-2">Admin</p>
+        {/* Div 3: Dropdown */}
+        <div className="flex items-center justify-end w-1/3 space-x-4 mr-4 cursor-pointer">
+          <Dropdown
+            menu={{ items: profileMenuItems }}
+            placement="bottomRight"
+          >
+            <div className="flex items-center">
+              <Avatar
+                src="https://via.placeholder.com/40"
+                style={{ border: "1px solid #B2F6E9" }}
+              />
+              <p className="m-0 ml-2">Admin</p>
+            </div>
+          </Dropdown>
+        </div>
+      </Header>
+
+      <Content className="w-full p-8 bg-[#fff] flex-grow">{children}</Content>
+      
+      <Footer className="flex items-center justify-between p-0 bg-[#A8E7DE] w-full h-[150px] pl-12 pr-12">
+        <div className="flex items-center justify-start w-1/3">
+          <img
+            src="../../../public/footer-logo.png"
+            alt="Logo da Thrift shop"
+            className="w-[40%]"
+          />
+        </div>
+        <div className="flex flex-col items-center w-1/3 mr-4 cursor-pointer">
+          <div>
+            <p>Conheça nossas políticas:</p>
           </div>
-        </Dropdown>
-      </div>
-    </Header>
-
-        <Content className="w-full h-full p-8 bg-[#fff]">{children}</Content>
-        <Footer className="flex items-center justify-between p-0 bg-[#A8E7DE] w-full">
-          Footer
-        </Footer>
-      </Layout>
+          <div className="flex flex-col items-center">
+            <p>Políticas de Frete</p>
+            <p>Políticas de Privacidade</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-end w-1/3 mr-4 cursor-pointer">
+          <div>
+            <p>Siga nossas redes sociais:</p>
+          </div>
+          <div className="flex space-x-4 mt-2">
+            <img
+              src="../../../public/instagram-icon.svg"
+              alt="Logo do Instagram da Thrift Shop"
+              className="w-8 h-8"
+            />
+            <img
+              src="../../../public/wpp-icon.svg"
+              alt="Logo do WhatsApp da Thrift Shop"
+              className="h-6 w-6"
+            />
+            <img
+              src="../../../public/youtube-icon.svg"
+              alt="Logo do YouTube da Thrift Shop"
+              className="w-8 h-8"
+            />
+          </div>
+        </div>
+      </Footer>
     </Layout>
   );
 };
--- src/pages/landingPage/Store/index.tsx
+++ src/pages/landingPage/Store/index.tsx
@@ -1,13 +1,145 @@
-import LayoutBaseLandingPage from '../../../components/LayoutBaseLandingPage';
+import { Button, Card, Carousel } from "antd";
+import LayoutBaseLandingPage from "../../../components/LayoutBaseLandingPage";
+
+const { Meta } = Card;
+
+const contentStyle: React.CSSProperties = {
+  margin: 0,
+  height: "200px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
+
+const StyleCarrosselCard: React.CSSProperties = {
+  margin: 0,
+  height: "420px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+};
 
 const Store = () => {
   return (
     <>
-    <LayoutBaseLandingPage>
-      <h1>Store Page</h1>
-    </LayoutBaseLandingPage>
+      <LayoutBaseLandingPage>
+        <h1>Store Page</h1>
+        <div className="w-full">
+          <Carousel arrows infinite={false} dots={false}>
+            <div>
+              <h3 style={contentStyle}>1</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>2</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>3</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>4</h3>
+            </div>
+          </Carousel>
+        </div>
+        <div className="w-full mt-8">
+          <div>
+            <div
+              className="flex justify-center space-x-4"
+              style={StyleCarrosselCard}
+            >
+              <Card
+                hoverable
+                style={{ width: 240, height: 400 }}
+                cover={
+                  <img
+                    alt="example"
+                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                  />
+                }
+              >
+                <Meta
+                  title="Camiseta Branca Esportiva - Nike"
+                  description="www.instagram.com"
+                />
+              </Card>
+
+              <Card
+                hoverable
+                style={{ width: 240, height: 400 }}
+                cover={
+                  <img
+                    alt="example"
+                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                  />
+                }
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+              <Card
+                hoverable
+                style={{ width: 240, height: 400 }}
+                cover={
+                  <img
+                    alt="example"
+                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                  />
+                }
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+
+              <Card
+                hoverable
+                style={{ width: 240, height: 400 }}
+                cover={
+                  <img
+                    alt="example"
+                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                  />
+                }
+              >
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full mt-8">
+          <div>
+            <div
+              className="flex justify-center space-x-4"
+              style={StyleCarrosselCard}
+            >
+              <Card
+                hoverable
+                style={{ width: 240, height: 400 }}
+                cover={
+                  <img
+                    alt="example"
+                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                  />
+                }
+              >
+                <Meta
+                  title="Camiseta Branca Esportiva - Nike"
+                  description={<Button>Comprar</Button>}
+                />
+              </Card>
+            </div>
+          </div>
+        </div>
+      </LayoutBaseLandingPage>
     </>
   );
-}
+};
 
 export default Store;
